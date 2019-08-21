d084e6680cad4aeb7b9cf030adb6308853182b75
Pascal Bach <pascal.bach@nextrem.ch>
2019-08-21 23:34:02 +0200

    Add example helper that reads credentials from environment variables

---
+++ examples/git-credential-env.rs
@@ -0,0 +1,21 @@
+/// A simple git credential helper that
+/// provides the credentials from the two environment variables
+/// `GIT_USER` and `GIT_PASS`.
+///
+/// Just copy the resulting binary `git-credential-env` into `PATH` and
+/// configure it using `git config credential.helper env`.
+use git_credential::GitCredential;
+use std::env;
+
+fn main() {
+    let mut gc = GitCredential::default();
+
+    // If we can't read a variable just ignore it.
+    gc.username = env::var("GIT_USER").ok();
+    gc.password = env::var("GIT_PASS").ok();
+
+    let out = std::io::stdout();
+
+    gc.to_writer(out)
+        .expect("Something went wrong writing the credentials!");
+}
